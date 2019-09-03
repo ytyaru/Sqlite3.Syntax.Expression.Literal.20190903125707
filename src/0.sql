976b77deff4f22b012c18857c38842b469d63803
ytyaru <ytyaru@outlook.jp>
2019-09-03 12:58:47 +0900

    初回コミット

---
+++ src/0.sql
@@ -0,0 +1,14 @@
+select 1;
+select 1.2;
+select 6.7E+3;
+select -9223372036854775808;
+select +9223372036854775807;
+select -9223372036854775809;
+select +9223372036854775808;
+select 0xF;
+select 'ABC';
+select x'FF';
+select NULL;
+select true;
+select false;
+
